c80291db30426df843d37e495f071970a35038c3
Camile Coelho <camilecoelho23@gmail.com>
2023-05-18 14:09:56 -0300

    TelaPrincipalForm corrigida.

--- E-Agenda.WinApp/TelaPrincipalForm.Designer.cs
+++ E-Agenda.WinApp/TelaPrincipalForm.Designer.cs
@@ -28,16 +28,189 @@
         /// </summary>
         private void InitializeComponent()
         {
+            menuStrip1 = new MenuStrip();
+            cadastrosMenuItem = new ToolStripMenuItem();
+            contatosMenuItem = new ToolStripMenuItem();
+            compromissosMenuItem = new ToolStripMenuItem();
+            tarefasMenuItem = new ToolStripMenuItem();
+            despesasToolStripMenuItem = new ToolStripMenuItem();
+            despesasMenuItem = new ToolStripMenuItem();
+            categoriasMenuItem = new ToolStripMenuItem();
+            relatóriosToolStripMenuItem = new ToolStripMenuItem();
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            toolStrip1 = new ToolStrip();
+            btnInserir = new ToolStripButton();
+            btnEditar = new ToolStripButton();
+            btnExcluir = new ToolStripButton();
+            toolStripSeparator1 = new ToolStripSeparator();
+            labelTipoDoCadastro = new ToolStripLabel();
+            panelRegistros = new Panel();
+            menuStrip1.SuspendLayout();
+            statusStrip1.SuspendLayout();
+            toolStrip1.SuspendLayout();
             SuspendLayout();
             // 
-            // Form1
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { cadastrosMenuItem, relatóriosToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // cadastrosMenuItem
+            // 
+            cadastrosMenuItem.DropDownItems.AddRange(new ToolStripItem[] { contatosMenuItem, compromissosMenuItem, tarefasMenuItem, despesasToolStripMenuItem });
+            cadastrosMenuItem.Name = "cadastrosMenuItem";
+            cadastrosMenuItem.Size = new Size(71, 20);
+            cadastrosMenuItem.Text = "Cadastros";
+            // 
+            // contatosMenuItem
+            // 
+            contatosMenuItem.Name = "contatosMenuItem";
+            contatosMenuItem.Size = new Size(154, 22);
+            contatosMenuItem.Text = "Contatos";
+            contatosMenuItem.Click += contatosMenuItem_Click;
+            // 
+            // compromissosMenuItem
+            // 
+            compromissosMenuItem.Name = "compromissosMenuItem";
+            compromissosMenuItem.Size = new Size(154, 22);
+            compromissosMenuItem.Text = "Compromissos";
+            // 
+            // tarefasMenuItem
+            // 
+            tarefasMenuItem.Name = "tarefasMenuItem";
+            tarefasMenuItem.Size = new Size(154, 22);
+            tarefasMenuItem.Text = "Tarefas";
+            tarefasMenuItem.Click += tarefasMenuItem_Click;
+            // 
+            // despesasToolStripMenuItem
+            // 
+            despesasToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { despesasMenuItem, categoriasMenuItem });
+            despesasToolStripMenuItem.Name = "despesasToolStripMenuItem";
+            despesasToolStripMenuItem.Size = new Size(154, 22);
+            despesasToolStripMenuItem.Text = "Despesas";
+            // 
+            // despesasMenuItem
+            // 
+            despesasMenuItem.Name = "despesasMenuItem";
+            despesasMenuItem.Size = new Size(130, 22);
+            despesasMenuItem.Text = "Despesas";
+            // 
+            // categoriasMenuItem
+            // 
+            categoriasMenuItem.Name = "categoriasMenuItem";
+            categoriasMenuItem.Size = new Size(130, 22);
+            categoriasMenuItem.Text = "Categorias";
+            // 
+            // relatóriosToolStripMenuItem
+            // 
+            relatóriosToolStripMenuItem.Name = "relatóriosToolStripMenuItem";
+            relatóriosToolStripMenuItem.Size = new Size(71, 20);
+            relatóriosToolStripMenuItem.Text = "Relatórios";
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1 });
+            statusStrip1.Location = new Point(0, 428);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(800, 22);
+            statusStrip1.TabIndex = 1;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(118, 17);
+            toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.Items.AddRange(new ToolStripItem[] { btnInserir, btnEditar, btnExcluir, toolStripSeparator1, labelTipoDoCadastro });
+            toolStrip1.Location = new Point(0, 24);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(800, 31);
+            toolStrip1.TabIndex = 2;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // btnInserir
+            // 
+            btnInserir.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btnInserir.Image = Properties.Resources.add_FILL0_wght400_GRAD0_opsz48;
+            btnInserir.ImageTransparentColor = Color.Magenta;
+            btnInserir.Name = "btnInserir";
+            btnInserir.Padding = new Padding(4);
+            btnInserir.Size = new Size(28, 28);
+            btnInserir.Text = "Inserir";
+            btnInserir.Click += btnInserir_Click;
+            // 
+            // btnEditar
+            // 
+            btnEditar.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btnEditar.ImageScaling = ToolStripItemImageScaling.None;
+            btnEditar.ImageTransparentColor = Color.Magenta;
+            btnEditar.Name = "btnEditar";
+            btnEditar.Padding = new Padding(7);
+            btnEditar.Size = new Size(23, 28);
+            btnEditar.Text = "Editar";
+            btnEditar.Click += btnEditar_Click;
+            // 
+            // btnExcluir
+            // 
+            btnExcluir.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btnExcluir.ImageScaling = ToolStripItemImageScaling.None;
+            btnExcluir.ImageTransparentColor = Color.Magenta;
+            btnExcluir.Name = "btnExcluir";
+            btnExcluir.Padding = new Padding(7);
+            btnExcluir.Size = new Size(23, 28);
+            btnExcluir.Text = "Excluir";
+            btnExcluir.Click += btnExcluir_Click;
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(6, 31);
+            // 
+            // labelTipoDoCadastro
+            // 
+            labelTipoDoCadastro.Name = "labelTipoDoCadastro";
+            labelTipoDoCadastro.Size = new Size(75, 28);
+            labelTipoDoCadastro.Text = "tipoCadastro";
+            // 
+            // panelRegistros
+            // 
+            panelRegistros.BorderStyle = BorderStyle.FixedSingle;
+            panelRegistros.Dock = DockStyle.Fill;
+            panelRegistros.Location = new Point(0, 55);
+            panelRegistros.Name = "panelRegistros";
+            panelRegistros.Size = new Size(800, 373);
+            panelRegistros.TabIndex = 3;
+            // 
+            // TelaPrincipalForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Name = "Form1";
-            Text = "Form1";
+            Controls.Add(panelRegistros);
+            Controls.Add(toolStrip1);
+            Controls.Add(statusStrip1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "TelaPrincipalForm";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "e-Agenda 1.0";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
